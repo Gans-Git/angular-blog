723c1b70c1d8613df9cdcf01e429af67339a745d
Gans-Git <bielandrade.snts@gmail.com>
2023-10-13 13:23:15 -0300

    blog criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "nova notícia",
+    "description": "ble ble",
+    "photoCover": "https://static.wikia.nocookie.net/naruto/images/a/af/Aoba-Yamashiro.png/revision/latest?cb=20181031213411&path-prefix=pt-br"
+  },
+  {
+    "id":"2",
+    "title": "nova notícia 2",
+    "description": "ble ble blahhh",
+    "photoCover": "https://gartic.com.br/imgs/mural/t0/t0bi_madara/tobi-e-o-aoba.png"
+  }
+]
